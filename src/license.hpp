abfd2dcda3febfdeeca1425e59f8eb1452dfdfd6
Nikolas Koesling <nikolas@koesling.info>
2024-01-29 19:26:16 +0100

    change license

--- src/license.hpp
+++ src/license.hpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2022 Nikolas Koesling <nikolas@koesling.info>.
- * This program is free software. You can redistribute it and/or modify it under the terms of the MIT License.
+ * This program is free software. You can redistribute it and/or modify it under the terms of the GPLv3 License.
  */
 
 #pragma once
